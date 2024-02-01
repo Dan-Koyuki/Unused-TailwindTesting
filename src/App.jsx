795ff855aaa0c6f800fab619bcd74e8c1949726c
Dan-Koyuki <dankoyuki398@gmail.com>
2024-02-01 16:35:10 +0700

    nyoba tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <nav className='sticky flex z-1'>
+        <p>test</p>
+      </nav>
+      <section className='h-screen bg-green-400'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem esse reprehenderit iusto molestiae! Libero unde debitis nisi, perferendis eaque et dolores quasi error sapiente laboriosam amet quia architecto facilis maiores.</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
